93f626b1b9a0832ed4f7b251487babf4b4bfcc38
Robert Williams <robert@increscent.org>
2021-12-04 23:15:07 -0700

    test is magically working now

--- tests/vector_sort.scm
+++ tests/vector_sort.scm
@@ -23,6 +23,4 @@
 (assert (vec-sorted? (sort! (list->vector '(1 2 3 4 5 6 7 8)) >) >=))
 (assert (vec-sorted? (sort! (list->vector '(92 59 30 57 74 78 43 33 77 10 78 83 76 49 42 94 82 70 15 11 90 86 44 70 39 64 69 30 59 95 15 79 13 54 98 82 42 96 79 17 56 93 20 1 84 72 75 19 74 43)) >) >=))
 (assert (vec-sorted? (sort! (list->vector '(92 59 30 57 74 78 43 33 77 10 78 83 76 49 42 94 82 70 15 11 90 86 44 70 39 64 69 30 59 95 15 79 13 54 98 82 42 96 79 17 56 93 20 1 84 72 75 19 74 43)) <) <=))
-
-; This fails with 'eval error: bad argument type' -- not sure why...
-;(assert (vec-sorted? (sort! (list->vector '(3 8 1 7 2 9 4 5)) <=) <=))
+(assert (vec-sorted? (sort! (list->vector '(3 8 1 7 2 9 4 5)) <=) <=))
